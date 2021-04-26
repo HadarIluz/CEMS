4729cde5effb072962041aeb9d077ba829391485
Yuval Hayam <12692573+yuval96@users.noreply.github.com>
2021-04-26 15:38:08 +0300

    implemented Request interface for getting the table.

---
+++ src/logic/TestTableRequest.java
@@ -0,0 +1,22 @@
+package logic;
+
+import java.util.ArrayList;
+
+public class TestTableRequest implements Request{
+	private ArrayList<TestRow> testTableData;
+
+	public TestTableRequest() {
+		super();
+		testTableData = new ArrayList<>();
+	}
+	
+	public void addRow(TestRow row) {
+		testTableData.add(row);
+	}
+	
+	public ArrayList<TestRow> getTableData() {
+		return testTableData;
+	}
+	
+	
+}
