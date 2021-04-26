35df96510f42e5f262c64f05aa2e3361ed884564
iluzhadar <iluzhadar@gmail.com>
2021-04-26 14:56:55 +0300

    add 2 class: TestFormController and MainFormController. Update FXMLs

---
+++ src/gui/MainFormController.java
@@ -0,0 +1,28 @@
+package gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Font;
+
+public class MainFormController {
+	
+	private TestFormController test_Fc;	
+	@FXML
+    private Button btnTest;
+
+    @FXML
+    private Button btnTable;
+
+    @FXML
+    private Font x1;
+
+    @FXML
+    private Font x3;
+    
+    
+    
+    
+    
+    
+
+}
---
+++ src/gui/TestFormController.java
@@ -0,0 +1,23 @@
+package gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Font;
+
+public class TestFormController {
+
+    @FXML
+    private Button btnTest;
+
+    @FXML
+    private Button btnTable;
+
+    @FXML
+    private Font x1;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private Font x3;
+}
