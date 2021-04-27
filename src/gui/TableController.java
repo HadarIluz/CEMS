fe8f0eadf7e8376752c4a5ce2d5f1ff5f65f189e
yadin <yadinamsalem@gmail.com>
2021-04-27 14:55:13 +0300

    Changes According updateTestTime & getTestTable methods(inDBController)

--- src/gui/TableController.java
+++ src/gui/TableController.java
@@ -1,10 +1,13 @@
 package gui;
-
+import client.ClientUI;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.text.Font;
+import logic.TestTableRequest;
 
 public class TableController {
+	private TestTableRequest tesTable; //Used for the test table shown on Screen
 
     @FXML
     private Button btnTest;
@@ -19,5 +22,17 @@ public class TableController {
     private Font x3;
     
     //lior
+    
+    @FXML
+    void pressTable(ActionEvent event) {
+    	ClientUI.chat.accept("Table"); //send message to get all table rows
+
+    }
+
+	public void loadTable(TestTableRequest table) {
+		
+		//
+		
+	}
     
 }
