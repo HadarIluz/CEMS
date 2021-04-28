551ff26293363d4c1cf4bd64f6b19297b08a3b60
iluzhadar <iluzhadar@gmail.com>
2021-04-28 17:05:34 +0300

    StatusMsg class

---
+++ src/logic/StatusMsg.java
@@ -0,0 +1,22 @@
+package logic;
+
+public class StatusMsg {
+	
+	private String status;
+	private String description = null;
+	
+	
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
