9c59ce7a464891a905d13307432425fcb9536f82
iluzhadar <iluzhadar@gmail.com>
2021-04-30 16:07:31 +0300

    Ready to server for checking description addition in StatusMsg class.

--- src/gui/TestController.java
+++ src/gui/TestController.java
@@ -22,10 +22,9 @@ import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import logic.UpdateDataRequest;
 
-@SuppressWarnings("unused")
 public class TestController {
 
-	public UpdateDataRequest upDataReq=new UpdateDataRequest();
+	public UpdateDataRequest upDataReq = new UpdateDataRequest();
 	// public TestTableRequest test_Treq;
 	// public StatusMsg status;
 
@@ -58,94 +57,81 @@ public class TestController {
 
 	@FXML
 	private Font x3;
-	
-  
 
 	/*
 	 * the function active when user press on save button.
-	 * 
 	 */
 	@FXML
 	public void pressUpdateTableReq(ActionEvent event) throws Exception {
-		String ExamID;
-		String TimeForTest;
-		// String text;
+		//String text;
 		// Clean
 		ReqFiledMessage1.setText("");
 		ReqFiledMessage2.setText("");
 		statusMessage.setText("");
 
 		// get the exam id`s number that user typed
-		ExamID = txtExamID.getText();
-		System.out.println(ExamID); // test
+		String ExamID = txtExamID.getText();
 		if (ExamID.trim().isEmpty()) {
-			System.out.println("You must enter an exam id number\n"); // message to console.
-			// ReqFiled functionality.
-
-			ReqFiledMessage1.setTextFill(Paint.valueOf("Red"));
-			// DEBUG: message from server not working...
-			// text = ChatClient.statusMsg.getDescription().toString();
-			// System.out.println(text);
-			// ReqFiledMessage1.setText(text);
-			ReqFiledMessage1.setText("examID is Req filed");
+			System.out.println("You must enter an exam id number"); // message to console.
+			showMsg(ReqFiledMessage1, "ExamID is Req field.");
+			//text = ChatClient.statusMsg.getDescription();
+			//System.out.println(text); //test
+			//showMsg(ReqFiledMessage1, text);
 		}
-		try {
-			TimeForTest = txtTimeForTest.getText();
-			System.out.println(TimeForTest); // test
-			if (TimeForTest.trim().isEmpty()) {
-				System.out.println("You must enter an time number\n"); // message to console.
-				// ReqFiled functionality.
-				ReqFiledMessage2.setTextFill(Paint.valueOf("Red"));
-				// text = ChatClient.statusMsg.getDescription().toString();
-				// ReqFiledMessage2.setText(text);
-				ReqFiledMessage2.setText("time is Req filed.");
-			}
-
-			// in case filed not empty checks if exist in DB
-			if (!ExamID.trim().isEmpty()) {
-
-				// send request to server to check if exam ID exist
-				// ClientUI.chat.accept("getRow " + ExamID);
-				System.out.println("line 106");
-
+		// try {
+		String TimeForTest = txtTimeForTest.getText();
+		if (TimeForTest.trim().isEmpty()) {
+			System.out.println("You must enter an time number"); // message to console.
+			showMsg(ReqFiledMessage2, "time is Req field.");
+			// text = ChatClient.statusMsg.getDescription();
+			// showMsg(ReqFiledMessage2, text);
+		}
+		// in case fields not empty checks if exist in DB
+		else if (!ExamID.trim().isEmpty()) {
+			try {
 				upDataReq.setExamID(ExamID);
 				upDataReq.setTimeAllotedForTest(TimeForTest);
 				ClientUI.chat.accept(upDataReq);
 
 				if (ChatClient.statusMsg.getStatus().equals("ERROR")) {
 					System.out.println("Exam ID Not Found"); // message to console.
-					// ReqFiled functionality.
-					// text = ChatClient.statusMsg.getDescription().toString();
-					statusMessage.setTextFill(Paint.valueOf("Red"));
-					// statusMessage.setText(text);
-					statusMessage.setText("Exam ID Not Found.");
+					showMsg(statusMessage, "Exam ID Not Found.");
+					// text = ChatClient.statusMsg.getDescription();
+					// showMsg(statusMessage, text);
 				}
-
+				// try {
 				// Handle a case ExamID found,
 				else {
-					System.out.println("Exam ID Found"); // message to console.
-					upDataReq.setExamID(ExamID);
-					upDataReq.setTimeAllotedForTest(TimeForTest);
-					// ClientUI.chat.accept("getRow " + upDataReq.toString());
+					try {
+						System.out.println("Exam ID Found"); // message to console.
+						upDataReq.setExamID(ExamID);
+						upDataReq.setTimeAllotedForTest(TimeForTest);
+						ClientUI.chat.accept(upDataReq);
 
-					ClientUI.chat.accept(upDataReq);
+						statusMessage.setTextFill(Paint.valueOf("Green"));
+						statusMessage.setText("updated.");
 
-					statusMessage.setTextFill(Paint.valueOf("Green"));
-					statusMessage.setText("updated.");
+						System.out.println(upDataReq.toString()); // message for console
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
 
-					System.out.println(upDataReq.toString()); // message for console
-				}
-			} // END else
+				} // END else1
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} // END else
 
-		} catch (Exception e) {
-			// return;
-		}
+	}
 
+	// show red commend to client.
+	private void showMsg(Label label, String msg) {
+		label.setTextFill(Paint.valueOf("Red"));
+		label.setText(msg);
 	}
 
 	@FXML
 	public void pressBtnTableForm(ActionEvent event) throws Exception {
-
 		try {
 			System.out.println("Table Fram Tool display"); // message to console.
 			((Node) event.getSource()).getScene().getWindow().hide(); // hiding primary(Test) window
