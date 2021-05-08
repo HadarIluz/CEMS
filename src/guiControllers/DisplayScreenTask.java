7242dc9ad337de69125566a717b813517b3035d0
iluzhadar <iluzhadar@gmail.com>
2021-05-08 23:21:08 +0300

    Update names and add FXMLLoader() to display all screens.

--- src/guiControllers/DisplayScreenTask.java
+++ src/guiControllers/DisplayScreenTask.java
@@ -14,11 +14,40 @@ public class DisplayScreenTask extends Application {
 			GridPane root = new GridPane();
 			Scene scene = new Scene(root, 1004, 626);
 			Pane newMnueLeft = FXMLLoader.load(getClass().getResource("StudentMenuLeft.fxml"));
+			//newMnueLef newMnueLeft = FXMLLoader.load(getClass().getResource("TeacherMenuLeft.fxml"));
+			//Pane newMnueLeft = FXMLLoader.load(getClass().getResource("PrincipalMenuLeft.fxml"));
 			root.add(newMnueLeft, 0, 0);
 			// root.getChildren().add(newMnueLeft);
 			// --
-
-			Pane newPaneRight = FXMLLoader.load(getClass().getResource("CreateExam_step1.fxml"));
+			
+			//STUDENT:
+			Pane newPaneRight = FXMLLoader.load(getClass().getResource("StartManualExam.fxml"));
+			//Pane newPaneRight = FXMLLoader.load(getClass().getResource("EnterAnExam.fxml"));
+			//Pane newPaneRight = FXMLLoader.load(getClass().getResource("SolveExam.fxml"));
+			//Pane newPaneRight = FXMLLoader.load(getClass().getResource("ViewExam.fxml"));
+			//Pane newPaneRight = FXMLLoader.load(getClass().getResource("CreateExam_step1.fxml"));
+			//Pane newPaneRight = FXMLLoader.load(getClass().getResource("CreateExam_step1.fxml"));
+			//Pane newPaneRight = FXMLLoader.load(getClass().getResource("CreateExam_step1.fxml"));
+			//Pane newPaneRight = FXMLLoader.load(getClass().getResource("CreateExam_step1.fxml"));
+			
+			//TEACHER:
+			//Pane newPaneRight = FXMLLoader.load(getClass().getResource("TeacherStatistics.fxml"));
+			//Pane newPaneRight = FXMLLoader.load(getClass().getResource("CreateExam_step1.fxml"));
+			//Pane newPaneRight = FXMLLoader.load(getClass().getResource("createExam_addQ_step2.fxml"));
+			//Pane newPaneRight = FXMLLoader.load(getClass().getResource("createNewExam_step3.fxml"));
+			//Pane newPaneRight = FXMLLoader.load(getClass().getResource("AddTimeToExam"));
+			//Pane newPaneRight = FXMLLoader.load(getClass().getResource("ScoreApproval.fxml"));
+			//Pane newPaneRight = FXMLLoader.load(getClass().getResource("EditExam.fxml"));
+			//Pane newPaneRight = FXMLLoader.load(getClass().getResource("TeacherStatistics.fxml"));
+			//Pane newPaneRight = FXMLLoader.load(getClass().getResource("CreateActiveExam.fxml"));
+			//Pane newPaneRight = FXMLLoader.load(getClass().getResource("CreateQuestion.fxml"));
+			//Pane newPaneRight = FXMLLoader.load(getClass().getResource("ExamBank.fxml"));
+			//Pane newPaneRight = FXMLLoader.load(getClass().getResource("QuestionBank.fxml"));
+			
+			//PRINCIPAL:
+			//Pane newPaneRight = FXMLLoader.load(getClass().getResource("PrincipalGetReports.fxml"));
+			//Pane newPaneRight = FXMLLoader.load(getClass().getResource("ApprovalTimeExtention.fxml"));
+			
 			root.add(newPaneRight, 1, 0);
 			// root.getChildren().add(newPaneRight);
 
