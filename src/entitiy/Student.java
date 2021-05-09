2b2e775a0d9f2b0b0ea6ee1fd1c28f9df2ece9e9
iluzhadar <iluzhadar@gmail.com>
2021-05-09 12:54:09 +0300

    Add Teacher-entity class. (extends User implements Serializable)

--- src/entitiy/Student.java
+++ src/entitiy/Student.java
@@ -6,19 +6,20 @@ import java.io.Serializable;
 @SuppressWarnings("serial")
 public class Student extends User implements Serializable {
 
-	/*private int id;
-	private String password;
-	private String StudentName; 
-	private String StudentEmail;
-	private boolean isLogged;
-	*/
+	/* private int id; 
+	 * private String password; 
+	 * private String StudentName; 
+	 * private
+	 * String StudentEmail; 
+	 * private boolean isLogged;
+	 */
 	private float studentAvg;
-	private int departmentID;
+	private int departmentID;  //In the story: Math==(02)
 
-	public Student(int id, String password, String studentName, String StudentEmail, boolean isLogged,float studentAvg, int departmentID) {
+	public Student(int id, String password, String studentName, String StudentEmail, boolean isLogged, float studentAvg, int departmentID) {
 		super(id, password, studentName, StudentEmail, isLogged);
-		this.setStudentAvg(studentAvg);
-		this.setDepartmentID(departmentID);
+		this.studentAvg = studentAvg;
+		this.departmentID = departmentID;
 	}
 
 	public float getStudentAvg() {
@@ -36,10 +37,10 @@ public class Student extends User implements Serializable {
 	public void setDepartmentID(int departmentID) {
 		this.departmentID = departmentID;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Student [studentID=" + this.getId() + ", studentAvg=" + studentAvg + ", departmentID=" + departmentID + "]";
+		return "Student [studentID=" + this.getId() + ", studentAvg=" + studentAvg + ", departmentID=" + departmentID+ "]";
 	}
 
 }
---
+++ src/entitiy/Teacher.java
@@ -0,0 +1,14 @@
+package entitiy;
+
+import java.io.Serializable;
+
+//Entity class - define Student in the CEMS system.
+@SuppressWarnings("serial")
+public class Teacher extends User implements Serializable {
+
+	public Teacher(int id, String password, String userName, String email, boolean isLogged) {
+		super(id, password, userName, email, isLogged);
+		// TODO Auto-generated constructor stub
+	}
+
+}
