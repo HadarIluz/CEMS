d6ad10933f61c7b0408cfbb6911de75f0ac94943
Yuval Hayam <12692573+yuval96@users.noreply.github.com>
2021-05-10 21:07:31 +0300

    added Question entity class

---
+++ src/entity/Question.java
@@ -0,0 +1,61 @@
+package entity;
+
+import java.io.Serializable;
+
+public class Question implements Serializable{
+
+	private String questionID;
+	private String question;
+	private String description;
+	private String[] answers;
+	private int correctAnswerIndex;
+	
+	public Question(String questionID) {
+		this.questionID = questionID;
+	}
+	
+	
+	
+	public Question(String questionID, String question, String description, String[] answers, int correctAnswerIndex) {
+		super();
+		this.questionID = questionID;
+		this.question = question;
+		this.description = description;
+		this.answers = answers;
+		this.correctAnswerIndex = correctAnswerIndex;
+	}
+
+
+
+	public String getQuestionID() {
+		return questionID;
+	}
+
+	public String getQuestion() {
+		return question;
+	}
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String[] getAnswers() {
+		return answers;
+	}
+	public void setAnswers(String[] answers) {
+		this.answers = answers;
+	}
+	public int getCorrectAnswerIndex() {
+		return correctAnswerIndex;
+	}
+	public void setCorrectAnswerIndex(int correctAnswerIndex) {
+		this.correctAnswerIndex = correctAnswerIndex;
+	}
+	
+	
+	
+}
