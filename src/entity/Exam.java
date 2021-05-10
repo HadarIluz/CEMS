68f23e00efa6fa4388dc43657a4db7527bd04e61
Yuval Hayam <12692573+yuval96@users.noreply.github.com>
2021-05-10 21:16:49 +0300

    created Exam entity class

---
+++ src/entity/Exam.java
@@ -0,0 +1,147 @@
+package entity;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Exam implements Serializable{
+
+	private String examID;
+	private Profession profession;
+	private Course course;
+	private int timeOfExam;
+	private ArrayList<Question> questions;
+	private ArrayList<Integer> questionScores;
+	private String commentForTeacher;
+	private String commentForStudents;
+	private Teacher author; // is this relevant or to delete?
+	
+	
+	
+	public Exam(String examID) {
+		super();
+		this.examID = examID;
+	}
+
+
+
+	public Exam(String examID, Profession profession, Course course, int timeOfExam, ArrayList<Question> questions,
+			ArrayList<Integer> questionScores, String commentForTeacher, String commentForStudents, Teacher author) {
+		super();
+		this.examID = examID;
+		this.profession = profession;
+		this.course = course;
+		this.timeOfExam = timeOfExam;
+		this.questions = questions;
+		this.questionScores = questionScores;
+		this.commentForTeacher = commentForTeacher;
+		this.commentForStudents = commentForStudents;
+		this.author = author;
+	}
+
+
+
+	public Profession getProfession() {
+		return profession;
+	}
+
+
+
+	public void setProfession(Profession profession) {
+		this.profession = profession;
+	}
+
+
+
+	public Course getCourse() {
+		return course;
+	}
+
+
+
+	public void setCourse(Course course) {
+		this.course = course;
+	}
+
+
+
+	public int getTimeOfExam() {
+		return timeOfExam;
+	}
+
+
+
+	public void setTimeOfExam(int timeOfExam) {
+		this.timeOfExam = timeOfExam;
+	}
+
+
+
+	public ArrayList<Question> getQuestions() {
+		return questions;
+	}
+
+
+
+	public void setQuestions(ArrayList<Question> questions) {
+		this.questions = questions;
+	}
+
+
+
+	public ArrayList<Integer> getQuestionScores() {
+		return questionScores;
+	}
+
+
+
+	public void setQuestionScores(ArrayList<Integer> questionScores) {
+		this.questionScores = questionScores;
+	}
+
+
+
+	public String getCommentForTeacher() {
+		return commentForTeacher;
+	}
+
+
+
+	public void setCommentForTeacher(String commentForTeacher) {
+		this.commentForTeacher = commentForTeacher;
+	}
+
+
+
+	public String getCommentForStudents() {
+		return commentForStudents;
+	}
+
+
+
+	public void setCommentForStudents(String commentForStudents) {
+		this.commentForStudents = commentForStudents;
+	}
+
+
+
+	public Teacher getAuthor() {
+		return author;
+	}
+
+
+
+	public void setAuthor(Teacher author) {
+		this.author = author;
+	}
+
+
+
+	public String getExamID() {
+		return examID;
+	}
+	
+	
+	
+	
+	
+}
