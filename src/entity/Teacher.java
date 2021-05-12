09f1bd0f4375e5ab3419ecfdd1ce6b87b2cf65b5
Yuval Hayam <12692573+yuval96@users.noreply.github.com>
2021-05-12 10:51:40 +0300

    Added profession list to teacher

--- src/entity/Teacher.java
+++ src/entity/Teacher.java
@@ -1,14 +1,28 @@
 package entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 //Entity class - define Student in the CEMS system.
 @SuppressWarnings("serial")
 public class Teacher extends User implements Serializable {
 
+	private ArrayList<Profession> professions;
+	
 	public Teacher(int id, String password, String userName, String email, UserType userType) {
 		super(id, password, userName, email, userType);
 		// TODO Auto-generated constructor stub
+		professions = new ArrayList<Profession>();
 	}
+
+	public ArrayList<Profession> getProfessions() {
+		return professions;
+	}
+
+	public void setProfessions(ArrayList<Profession> professions) {
+		this.professions = professions;
+	}
+	
+	
 	
 }
