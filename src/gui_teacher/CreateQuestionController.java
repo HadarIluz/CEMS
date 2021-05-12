0b2b80a11f26a183e10ca4baafaa25a849c0df42
Yuval Hayam <12692573+yuval96@users.noreply.github.com>
2021-05-12 15:41:45 +0300

    add imports to create question controller

--- src/gui_teacher/CreateQuestionController.java
+++ src/gui_teacher/CreateQuestionController.java
@@ -1,11 +1,13 @@
 package gui_teacher;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 
 public class CreateQuestionController {
