95bfd3b7770f8267acf4a07dad0e7680058cf267
iluzhadar <iluzhadar@gmail.com>
2021-05-16 14:40:24 +0300

    small change before branch (add initializable)

--- src/gui_principal/ApprovalTimeExtentionController.java
+++ src/gui_principal/ApprovalTimeExtentionController.java
@@ -1,16 +1,21 @@
 package gui_principal;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import entity.Exam;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 
-public class ApprovalTimeExtentionController {
+public class ApprovalTimeExtentionController implements Initializable{
 
     @FXML
-    private ComboBox<?> selectExam;
+    private ComboBox<String> selectExam;
 
     @FXML
     private Label lblAdditionalTime;
@@ -24,9 +29,23 @@ public class ApprovalTimeExtentionController {
     @FXML
     private Button btnApprove;
 
+    private Exam selectedExam;
+
     @FXML
     void selectExam(ActionEvent event) {
+    	}
 
+    
+    
+    public void loadExamToCombobox() {
+    	
     }
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		selectedExam=null;
+		
+	}
 
 }
