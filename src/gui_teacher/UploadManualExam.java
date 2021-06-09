8bbcddb5a1b2fad0ab2b1ea0a0b9ff2fa1e64cfd
Yuval Hayam <12692573+yuval96@users.noreply.github.com>
2021-06-09 19:26:39 +0300

    design of new screen

---
+++ src/gui_teacher/UploadManualExam.java
@@ -0,0 +1,46 @@
+package gui_teacher;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class UploadManualExam{
+
+    @FXML
+    private ImageView imgNotification;
+
+    @FXML
+    private Button btnBrowse;
+
+    @FXML
+    private TextField txtPath;
+
+    @FXML
+    private Button btnNext;
+
+    @FXML
+    private Button btnBack;
+
+    @FXML
+    void btnBack(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNext(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickBroswe(ActionEvent event) {
+
+    }
+
+
+}
