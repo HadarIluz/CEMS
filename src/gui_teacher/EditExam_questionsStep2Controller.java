d4bb9099609355c719514d728aa4cc5592f7f215
iluzhadar <iluzhadar@gmail.com>
2021-06-11 13:29:31 +0300

    update edit exam step 2

--- src/gui_teacher/EditExam_questionsStep2Controller.java
+++ src/gui_teacher/EditExam_questionsStep2Controller.java
@@ -1,8 +1,8 @@
 package gui_teacher;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.ResourceBundle;
 
 import client.CEMSClient;
@@ -18,7 +18,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
@@ -28,6 +30,7 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 import logic.RequestToServer;
 
 /**
@@ -90,14 +93,12 @@ public class EditExam_questionsStep2Controller extends GuiCommon implements Init
 	public static Exam exam;
 	private static Teacher teacher;
 	private static User principal;
-	private static boolean displayPrincipalView = false;
+	private static boolean displayPrincipalView;
 
 	//
-	private static ArrayList<Question> availableQuestions;
+	private static ArrayList<QuestionInExam> existsQuestions;
 	private ObservableList<QuestionInExamRow> selectedQuestionsRows = FXCollections.observableArrayList();
 	private ObservableList<QuestionInExamRow> Qlist;
-
-
 
 //	@FXML
 //	void DeleteFromExam(ActionEvent event) {
@@ -123,12 +124,10 @@ public class EditExam_questionsStep2Controller extends GuiCommon implements Init
 		}
 	}
 
-	
-	
-	
+//	//return:
 //	@FXML
 //	void btnBrowseQuestions(ActionEvent event) {
-//		BrowseQuestionController.setAvailableQuestions(availableQuestions);
+//		BrowseQuestionController.setAvailableQuestions(existsQuestions);
 //		FXMLLoader loader = new FXMLLoader(getClass().getResource("BrowseQuestions.fxml"));
 //		Scene newScene;
 //		try {
@@ -163,9 +162,7 @@ public class EditExam_questionsStep2Controller extends GuiCommon implements Init
 		}
 
 	}
-	
-	
-	
+
 	private void setQuestionsInNewExam() {
 		ArrayList<QuestionInExam> finaleQusetionList = new ArrayList();
 		for (QuestionInExamRow q : selectedQuestionsRows) {
@@ -173,7 +170,6 @@ public class EditExam_questionsStep2Controller extends GuiCommon implements Init
 		}
 		exam.setExamQuestionsWithScores(finaleQusetionList);
 	}
-	
 
 	/**
 	 * method set text of questionID when user select a question row from table
@@ -183,6 +179,9 @@ public class EditExam_questionsStep2Controller extends GuiCommon implements Init
 	@FXML
 	void chooseQ(MouseEvent event) {
 		Qlist = tableQuestion.getSelectionModel().getSelectedItems();
+		if (Qlist.isEmpty()) {
+			return;
+		}
 		txtChangeScore.setText(String.valueOf(Qlist.get(0).getScore()));
 		ChosenQuestionID.setText(Qlist.get(0).getQuestionID());
 
@@ -194,42 +193,37 @@ public class EditExam_questionsStep2Controller extends GuiCommon implements Init
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		txtChangeScore.setText("0");
-			
 		// bring all exam details (also questions and scores)
+
+		selectedQuestionsRows = FXCollections.observableArrayList();// NEW
 		RequestToServer req = new RequestToServer("getFullExamDetails");
 		req.setRequestData(exam);
 		ClientUI.cems.accept(req);
 
-		exam= (Exam) CEMSClient.responseFromServer.getResponseData();
-		
-		
-		
-		if (ClientUI.loggedInUser.getUser() instanceof Teacher) {
+		exam = (Exam) CEMSClient.responseFromServer.getResponseData();
+		existsQuestions = exam.getExamQuestionsWithScores(); // Return ArrayList<QuestionInExam>
+
+		if (!displayPrincipalView) {
 			teacher = (Teacher) ClientUI.loggedInUser.getUser();
 			txtChangeScore.setText("0");
 			textErrorMsg.setVisible(false); // when exam open at first for edit the total score is 100 !.
-			// initTableRows_getFronmServer(); //TODO: NOT WORKING!!
 
-			//initTableCols(); //HERE?
-			
+			//(TEST IT:)verify teacher not delete all of this questions before she edit this exam!
 			if (exam.getExamQuestionsWithScores() != null) {
-				for (QuestionInExam q : exam.getExamQuestionsWithScores()) {
-					System.out.println("!");
-					//availableQuestions.remove(q.getQuestion());   //DELETE
-					q.getQuestion();
-					insertRow(q);//DEBUG HERE!!
-				}
+				initTableCols(); 
 			}
-			initTableCols();//NEW
+//			if (exam.getExamQuestionsWithScores() != null) {
+//				for (QuestionInExam q : exam.getExamQuestionsWithScores()) {
+//					insertRow(q);
+//				}
+//			}
+//			//initTableCols();//NEW
 
 		} else if (ClientUI.loggedInUser.getUser() instanceof User) {
 			// setUp before load screen.
 			principal = (User) ClientUI.loggedInUser.getUser();
 			displayPrincipalView = true;
 
-//			btnDelete.setDisable(false);
-//			btnDelete.setVisible(false);
 			btnUpdateScore.setDisable(false);
 			btnUpdateScore.setVisible(false);
 			txtChangeScore.setDisable(false);
@@ -254,47 +248,53 @@ public class EditExam_questionsStep2Controller extends GuiCommon implements Init
 	private void insertRow(QuestionInExam q) {
 		selectedQuestionsRows.add(
 				new QuestionInExamRow(q.getQuestion().getQuestionID(), q.getScore(), q.getQuestion().getQuestion(), q));
-		//tableQuestion.refresh();
-		//availableQuestions.remove(q.getQuestion());
+		// tableQuestion.refresh();
+		existsQuestions.add(q);
+		// existsQuestions.remove(q.getQuestion());
 		updateTotalScore();
 	}
-	
-	
+
+	@SuppressWarnings("unchecked")
 	public void initTableCols() {
 
-		//tableQuestion.getColumns().clear();
+		// tableQuestion.getColumns().clear(); //DEBUG
 		questionID.setCellValueFactory(new PropertyValueFactory<>("questionID"));
 		questionScore.setCellValueFactory(new PropertyValueFactory<>("score"));
 		question.setCellValueFactory(new PropertyValueFactory<>("question"));
+
+		// create QuestionInExamRowToInsertIntoTable:
+		// run on all ArrayList<QuestionInExam> of the exists questions that inckude in
+		// this exam!
+		for (QuestionInExam q : existsQuestions) {
+			QuestionInExamRow newQuestionInExamRow = new QuestionInExamRow(q.getQuestion().getQuestionID(),
+					q.getScore(), q.getQuestion().getQuestion(), q);
+			selectedQuestionsRows.add(newQuestionInExamRow);
+		}
 
 		//tableQuestion.setItems(selectedQuestionsRows);
 		tableQuestion.getColumns().addAll(questionID, questionScore, question);
 
 	}
-	
-	
-	
-	
 
-	// TODO : !!!!!!!!!!
-	public void initTableRows_getFromServer() {
-		// get Questions for the examID that teacher selected in the Exam Back.
-		RequestToServer req = new RequestToServer("getQuestionsByIDForEditExam");
-		req.setRequestData(exam.getExamID());
-		HashMap<String, Question> allQuestionInExam = new HashMap<String, Question>();
-
-		ClientUI.cems.accept(req);
-		allQuestionInExam = ((HashMap<String, Question>) CEMSClient.responseFromServer.getResponseData());
-
+//	// TODO : !!!!!!!!!!
+//	public void initTableRows_getFromServer() {
+//		// get Questions for the examID that teacher selected in the Exam Back.
+//		RequestToServer req = new RequestToServer("getQuestionsByIDForEditExam");
+//		req.setRequestData(exam.getExamID());
+//		HashMap<String, Question> allQuestionInExam = new HashMap<String, Question>();
 //
-		ArrayList<QuestionInExam> finaleQusetionOfExamList = new ArrayList();
-		for (QuestionInExamRow q : selectedQuestionsRows) {
-			// allQuestionInExam.getOrDefault(finaleQusetionOfExamList, null) XXX
-
-			finaleQusetionOfExamList.add(q.getQuestionObject());
-		}
-		exam.setExamQuestionsWithScores(finaleQusetionOfExamList);
-	}
+//		ClientUI.cems.accept(req);
+//		allQuestionInExam = ((HashMap<String, Question>) CEMSClient.responseFromServer.getResponseData());
+//
+////
+//		ArrayList<QuestionInExam> finaleQusetionOfExamList = new ArrayList();
+//		for (QuestionInExamRow q : selectedQuestionsRows) {
+//			// allQuestionInExam.getOrDefault(finaleQusetionOfExamList, null) XXX
+//
+//			finaleQusetionOfExamList.add(q.getQuestionObject());
+//		}
+//		exam.setExamQuestionsWithScores(finaleQusetionOfExamList);
+//	}
 
 	public static void setnextScreenData(Exam examData, boolean displayPrincipalView2) {
 		exam = examData;
