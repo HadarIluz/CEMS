615fadda443f70c7ae18b1a5146298178ee27b68
liorkarish <liorkarish@gmail.com>
2021-06-14 11:17:44 +0300

    addition

--- src/gui_teacher/EditExamController.java
+++ src/gui_teacher/EditExamController.java
@@ -39,7 +39,7 @@ import logic.RequestToServer;
  * Teacher - editing permissions as described.
  * 
  * @author Hadar Iluz
- *
+ *@author lior karish
  */
 @SuppressWarnings("unused")
 public class EditExamController extends GuiCommon implements Initializable {
