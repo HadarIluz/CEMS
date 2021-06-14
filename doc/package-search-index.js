0ecda3197fd07d2eafdfa661331fe305a28a7ab9
Yuval Hayam <12692573+yuval96@users.noreply.github.com>
2021-06-14 13:57:10 +0300

    delete bad javadoc

--- doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"client"},{"l":"common"},{"l":"entity"},{"l":"gui_cems"},{"l":"gui_principal"},{"l":"gui_server"},{"l":"gui_student"},{"l":"gui_teacher"},{"l":"logic"},{"l":"Server"}]
